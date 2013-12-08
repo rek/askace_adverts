d4e296fc08a25d0c41255c3d4db5001d2dd7e109
Stephen Chernishov <unitlink@spalding.dreamhost.com>
2013-12-07 22:48:36 -0800

    almost finished admin dev round 1

--- adverts-add.php
+++ adverts-add.php
@@ -1,86 +1,128 @@
-<?php
+<style>
+  form label{ display: block; }
+  .error{ color: red; margin-top: 10px; }
+</style>
+
+<?php $edit = isset($_GET['id']); // check mode, add or edit ?>
+
+<div class="wrap">
+  <?php if ($edit) {
+    echo '<h2>Edit Advert ' . $_GET['id'] . '</h2>';
+  } else {
+    echo '<h2>Add New Advert</h2>';
+  } ?>
+</div>
+
+<?php $ok = false;
 
 if(isset($_POST['submit'])) {
-  //require_once('../../../wp-config.php');
   global $wpdb;
 
-  if(trim($_POST['heading']) != '' ||
-     trim($_POST['url']) != '' ||
-     trim($_POST['supplier']) != ''){
+  // form validation
+  $validated = true;
+  if (
+     trim($_POST['heading']) == '' ||
+     trim($_POST['url']) == '' ||
+     trim($_POST['supplier']) == ''
+  ) { $validated = false; }
 
-    $wpdb->insert( $wpdb->askace_adverts_table_name, array( 
-      'heading'    => $_POST['heading'], 
-      'summary'    => $_POST['summary'],
-      'url'        => $_POST['url'],
-      'supplier'   => $_POST['supplier'],
-      'email'      => $_POST['email'],
-      'payment'    => $_POST['payment'],
-      'layouttype' => $_POST['layouttype'],
-      'created_at' => current_time('mysql')
-    ) );
+  if ($validated) {
+    if ($edit) {
+      $wpdb->update( $wpdb->askace_adverts_table_name, array(
+            'heading'    => $_POST['heading'],
+            'summary'    => $_POST['summary'],
+            'url'        => $_POST['url'],
+            'supplier'   => $_POST['supplier'],
+            'email'      => $_POST['email'],
+            'payment'    => $_POST['payment'],
+            'layouttype' => $_POST['layouttype'],
+      ), array( 'ID' => $_POST['id'] ), array( '%s', '%s', '%s', '%s', '%s', '%d', '%s' ), array( '%d' ) );
 
-    echo '<p class="updated">Advert Inserted</p>';
+      echo '<div class="updated"><p>Advert Updated</p></div>';
+    } else {
+      $wpdb->insert( $wpdb->askace_adverts_table_name, array(
+        'heading'    => $_POST['heading'],
+        'summary'    => $_POST['summary'],
+        'url'        => $_POST['url'],
+        'supplier'   => $_POST['supplier'],
+        'email'      => $_POST['email'],
+        'payment'    => $_POST['payment'],
+        'layouttype' => $_POST['layouttype'],
+        'created_at' => current_time('mysql')
+      ) );
+
+      echo '<div class="updated"><p>Advert Added</p></div>';
+
+    }
   } else {
-    echo '<em class="error">There seems to be an error in the form. Please check all fields again</em>';
-
+    echo '<div class="updated"><p><em class="error">There seems to be an error in the form. Please check all fields again.</em></p></div>';
+    $results = $_POST;
   }
 
+  $ok = true;
+
+}
+
+if(isset($_GET['id'])) {
+
+  $results = $wpdb->get_results( 'select * from ' . $wpdb->askace_adverts_table_name . ' where id = ' . $_GET['id'] );
+  if(sizeof($results)>0) {
+    $ok = true;
+    $results = $results[0];
+  }
 }
 
 ?>
 
-<style>
-  .addForm label{ display: block; }
-  .error{ color: red; margin-top: 10px; }
-</style>
 
-<div class="wrap">  
-    <?php    echo "<h2>" . 'Add new Advert' . "</h2>"; ?> 
-</div>
 
-<form class="addForm" name="addForm" action="" method="post">
-<div>
-  <p>
-    <label>
-      <big>*Heading:</big>
-    </label>
-    <input name="heading" value="" size="30">
-  </p>
-  <p>
-    <label><strong>Product Summary:</strong>
-    </label>
-    <textarea name="summary" value="" rows="5" cols="50"></textarea>
-  </p>
-  <p>
-    <label style="color:blue">*<u>Website Link:</u>
-    </label>
-    <input name="url" value="" size="30">
-  </p>
-  <p>
-    <label>*<i>Name of Supplier:</i>
-    </label>
-    <input name="supplier" value="" size="30">
-  </p>
-  <p>
-    <label style="color:#ff2200"><u>Email:</u>
-    </label>
-    <input name="email" value="" size="30">
-  </p>
-  <p>
-    <label>Payment:</label>
-    <input name="payment" value="" size="30">
-  </p>
-  <p>
-    <label>Layout Type:</label>
-    <input name="layouttype" value="" size="30">
-  </p>
-</div>
-<div>
-  <input type="submit" name="submit" value="Add" />
-<?php
-$page     = 'askace_adverts/adverts-admin.php';
-$view_url = add_query_arg(compact('page'), admin_url('admin.php'));
-?>
-  <a href="<?php echo $view_url; ?>">View All</a>
-</div>
+<form action="" method="post">
+  <div>
+    <p>
+      <label>
+        <big>*Heading:</big>
+      </label>
+      <input name="heading" value="<?php echo $results->heading; ?>" size="30">
+      <input name="id" value="<?php if($edit) echo $results->id; ?>" type="hidden">
+    </p>
+    <p>
+      <label><strong>Product Summary:</strong>
+      </label>
+      <textarea name="summary" rows="5" cols="50"><?php echo $results->summary; ?></textarea>
+    </p>
+    <p>
+      <label style="color:blue">*<u>Website Link:</u>
+      </label>
+      <input name="url" value="<?php echo $results->url; ?>" size="30">
+    </p>
+    <p>
+      <label>*<i>Name of Supplier:</i>
+      </label>
+      <input name="supplier" value="<?php echo $results->supplier; ?>" size="30">
+    </p>
+    <p>
+      <label style="color:#ff2200"><u>Email:</u>
+      </label>
+      <input name="email" value="<?php echo $results->email; ?>" size="30">
+    </p>
+    <p>
+      <label>Payment:</label>
+      <input name="payment" value="<?php echo $results->payment; ?>" size="30">
+    </p>
+    <p>
+      <label>Layout Type:</label>
+      <input name="layouttype" value="<?php echo $results->layouttype; ?>" size="30">
+    </p>
+  </div>
+  <div>
+    <input type="submit" name="submit" value="<?php echo $edit ? 'Update' : 'Add'; ?>" />
+    <?php if ($edit) {
+        $delete_url = add_query_arg(array('page'=>'askace_adverts/adverts-admin.php', 'id'=>$results->id, 'delete'=>'true'), admin_url('admin.php'));
+        echo '<a onclick="return confirm(\'Are you sure you want to delete this advert?\')" href="'.$delete_url.'">Delete</a>';
+      }
+      $page     = 'askace_adverts/adverts-admin.php';
+      $view_url = add_query_arg(compact('page'), admin_url('admin.php'));
+    ?>
+    <a href="<?php echo $view_url; ?>">View All</a>
+  </div>
 </form>
--- adverts-admin.php
+++ adverts-admin.php
@@ -1,13 +1,18 @@
+<div class="wrap">
+
 <?php
-global $wpdb;
- 
-$pagenum = isset( $_GET['pagenum'] ) ? absint( $_GET['pagenum'] ) : 1;
-$limit = 5;
-$offset = ( $pagenum - 1 ) * $limit;
-$entries = $wpdb->get_results( "SELECT * FROM ".$wpdb->askace_adverts_table_name." LIMIT $offset, $limit" );
- 
-echo '<div class="wrap">';
- 
+    global $wpdb;
+
+    if (isset($_GET['delete']) && isset($_GET['id'])) {
+        $wpdb->delete( $wpdb->askace_adverts_table_name, array( 'ID' => $_GET['id'] ) );
+        echo '<div class="updated"><p>Advert #'.$_GET['id'].' Deleted</p></div>';
+    }
+     
+    $pagenum = isset( $_GET['pagenum'] ) ? absint( $_GET['pagenum'] ) : 1;
+    $limit = 5;
+    $offset = ( $pagenum - 1 ) * $limit;
+    $entries = $wpdb->get_results( "SELECT * FROM ".$wpdb->askace_adverts_table_name." LIMIT $offset, $limit" );
+     
 ?>
 
 <h1>View All Adverts</h1>
@@ -37,28 +42,25 @@ echo '<div class="wrap">';
         <?php if( $entries ) { ?>
  
             <?php
-            $count = 1;
-            $class = '';
-            foreach( $entries as $entry ) {
-                $class = ( $count % 2 == 0 ) ? ' class="alternate"' : '';
+                $count = 1;
+                $class = '';
+                foreach( $entries as $entry ) {
+                    $class = ( $count % 2 == 0 ) ? ' class="alternate"' : '';
             ?>
-<?php
-$page   = 'askace_adverts/adverts-edit.php';
-$id     = $entry->id;
-$advert_url = add_query_arg(compact('page', 'id'), admin_url('admin.php'));
-?>
+            <?php
+                $page = 'askace_adverts/adverts-add.php';
+                $id   = $entry->id;
+                $advert_url = add_query_arg(compact('page', 'id'), admin_url('admin.php'));
+            ?>
             <tr<?php echo $class; ?>>
-                <td><?php echo $entry->id; ?></td>
-                <td><a href="<?php echo $advert_url; ?>"><?php echo $entry->heading; ?></a></td>
+                <td><a href="<?php echo $advert_url; ?>"><?php echo $entry->id; ?></a></td>
+                <td><?php echo $entry->heading; ?></td>
                 <td><?php echo $entry->url; ?></td>
                 <td><?php echo $entry->supplier; ?></td>
                 <td><?php echo $entry->created_at; ?></td>
             </tr>
  
-            <?php
-                $count++;
-            }
-            ?>
+            <?php $count++; } ?>
  
         <?php } else { ?>
         <tr>
@@ -68,21 +70,25 @@ $advert_url = add_query_arg(compact('page', 'id'), admin_url('admin.php'));
     </tbody>
 </table>
  
-<?
- 
-$total = $wpdb->get_var( "SELECT COUNT(`id`) FROM {$wpdb->prefix}askace_adverts" );
-$num_of_pages = ceil( $total / $limit );
-$page_links = paginate_links( array(
-    'base' => add_query_arg( 'pagenum', '%#%' ),
-    'format' => '',
-    'prev_text' => __( '&laquo;', 'aag' ),
-    'next_text' => __( '&raquo;', 'aag' ),
-    'total' => $num_of_pages,
-    'current' => $pagenum
-) );
- 
-if ( $page_links ) {
-    echo '<div class="tablenav"><div class="tablenav-pages" style="margin: 1em 0">' . $page_links . '</div></div>';
-}
- 
-echo '</div>';
+<?php
+     
+    $total = $wpdb->get_var( "SELECT COUNT(`id`) FROM {$wpdb->prefix}askace_adverts" );
+    $num_of_pages = ceil( $total / $limit );
+    $page_links = paginate_links( array(
+        'base' => add_query_arg( 'pagenum', '%#%' ),
+        'format' => '',
+        'prev_text' => __( '&laquo;', 'aag' ),
+        'next_text' => __( '&raquo;', 'aag' ),
+        'total' => $num_of_pages,
+        'current' => $pagenum
+    ) );
+     
+    if ( $page_links ) {
+        echo '<div class="tablenav"><div class="tablenav-pages" style="margin: 1em 0">' . $page_links . '</div></div>';
+    }
+
+    $page     = 'askace_adverts/adverts-add.php';
+    $add_url = add_query_arg(compact('page'), admin_url('admin.php'));
+?>
+    <div><p><a href="<?php echo $add_url; ?>">Add</a></p></div>
+</div>
